b436eb88587422bd887a22e0397a892122223c9c
Ruzlex <alexruzanov2004@gmail.com>
2025-06-06 02:54:25 +0500

    правки

--- app/database.py
+++ app/database.py
@@ -5,9 +5,10 @@ SQLALCHEMY_DATABASE_URL = "postgresql://birzha:123@localhost/postgres"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    pool_size=5,
-    max_overflow=10,
-    pool_timeout=30
+    pool_size=20,
+    max_overflow=40,
+    pool_timeout=30,
+    pool_recycle=1800
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
